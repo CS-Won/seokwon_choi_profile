5378d48baedb6324193f3db2eb09365b07d0b0b6
CS-Won <seokwon0106@yonsei.ac.kr>
2026-03-01 03:31:32 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
-// const nextConfig = {
-//   output: "export",
-//   basePath: "/seokwon_choi_profile",
-//   assetPrefix: "/seokwon_choi_profile/",
-//   images: { unoptimized: true }
-// };
-
-// export default nextConfig;
-// next.config.mjs
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/seokwon_choi_profile",
+  assetPrefix: "/seokwon_choi_profile/",
+  images: { unoptimized: true }
+};
 
 export default nextConfig;
+// next.config.mjs
+/** @type {import('next').NextConfig} */
