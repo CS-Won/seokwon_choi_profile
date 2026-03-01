2b9ebb8e0c31d36edd24ba7589b4b4bc2eb24109
CS-Won <seokwon0106@yonsei.ac.kr>
2026-03-01 03:23:59 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
+// next.config.mjs 또는 next.config.js
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/seokwon_choi_profile",
-  assetPrefix: "/seokwon_choi_profile/",
-  images: { unoptimized: true }
+  experimental: {
+    turbopack: {
+      // 현재 디렉터리를 강제로 루트로 지정합니다.
+      root: "./",
+    },
+  },
 };
 
 export default nextConfig;
